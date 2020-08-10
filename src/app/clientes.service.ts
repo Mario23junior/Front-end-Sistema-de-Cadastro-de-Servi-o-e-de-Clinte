2354d2bcb07bb9aa9e3d112b407c166c51d7c1c1
Mario23junior <mariojunior3251@gmail.com>
2020-08-10 14:48:37 -0300

    Criando e injetando o service de clientes

---
+++ src/app/clientes.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './clientes/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor() { }
+
+  
+  /*
+   Teste
+   *
+   *
+   getCliente() : Cliente {
+     let cliente : Cliente = new Cliente(); 
+     cliente.nome  = "cODE";
+     cliente.cpf = "3292838";
+     return cliente;
+   }
+   */
+  
+}
