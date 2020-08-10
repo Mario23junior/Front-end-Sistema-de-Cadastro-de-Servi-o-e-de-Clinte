45c6b718a752261eb95ee61919ffb48814695768
Mario23junior <mariojunior3251@gmail.com>
2020-08-10 15:31:38 -0300

    Salvando clientes atraves da aplicacao web para a REST API

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
 import { Cliente } from './clientes/cliente';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  constructor( private http: HttpClient) {
 
-  
-  /*
-   Teste
-   *
-   *
+    }
+    
+    //salvando dados no banco de dados 
+    salvar( cliente: Cliente ) : Observable<Cliente> {
+        return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente)
+    }
+
    getCliente() : Cliente {
      let cliente : Cliente = new Cliente(); 
      cliente.nome  = "cODE";
      cliente.cpf = "3292838";
      return cliente;
    }
-   */
+   
   
 }
