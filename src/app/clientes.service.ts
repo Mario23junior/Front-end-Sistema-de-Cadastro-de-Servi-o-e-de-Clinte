7e822d4c52116cf545c09ba97e5912b66e72fafa
Mario23junior <mariojunior3251@gmail.com>
2020-08-11 06:37:53 -0300

    Criando componente de listagem de clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -9,13 +9,26 @@ import { Observable } from 'rxjs';
 })
 export class ClientesService {
 
-  constructor( private http: HttpClient) {
-
-    }
+  constructor( private http: HttpClient) {}
     
     //salvando dados no banco de dados 
     salvar( cliente: Cliente ) : Observable<Cliente> {
         return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente)
     }  
+
+    // Listando clientes do banco de dados 
+    //getClientes() : Observable<Cliente[]> {
+    //   return null;
+    //}
+
+    getClientes() : Cliente[]{
+       let cliente = new Cliente();
+       cliente.id = 1;
+       cliente.nome = "Fulano";
+       cliente.cpf = '34343434343';
+       cliente.dataCadastro = '18/04/2020';
+       return[cliente]
+
+    }
   
 }
