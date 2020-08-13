efe315098e048e37e3e2d978647e392050c9d3ca
Mario23junior <mariojunior3251@gmail.com>
2020-08-13 04:21:39 -0300

    Criando template de deleção de clientes

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router'
 export class ClientesListaComponent implements OnInit {
   
   clientes: Cliente[] = [];
+  clienteSelecionado:Cliente;
 
   constructor(
      private service: ClientesService,
@@ -26,5 +27,7 @@ export class ClientesListaComponent implements OnInit {
      this.router.navigate(['/clientes-form'])
    }
 
-  
+   preparaDelecao(cliente : Cliente){
+       this.clienteSelecionado = cliente;
+    }
 }
