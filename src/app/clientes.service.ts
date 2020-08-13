5d5828499337e69d53b32d4d265a396a75959214
Mario23junior <mariojunior3251@gmail.com>
2020-08-13 00:46:12 -0300

    Listando todos os clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -16,19 +16,10 @@ export class ClientesService {
         return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente)
     }  
 
-    // Listando clientes do banco de dados 
-    //getClientes() : Observable<Cliente[]> {
-    //   return null;
-    //}
-
-    getClientes() : Cliente[]{
-       let cliente = new Cliente();
-       cliente.id = 1;
-       cliente.nome = "Fulano";
-       cliente.cpf = '34343434343';
-       cliente.dataCadastro = '18/04/2020';
-       return[cliente]
-
+    //Listando todos clientes do banco de dados 
+    getClientes() : Observable<Cliente[]> {
+       return this.http.get<Cliente[]>("http://localhost:8080/api/clientes")
     }
+ 
   
 }
