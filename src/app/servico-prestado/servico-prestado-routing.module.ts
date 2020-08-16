208228fad21b82d9b284a18454bf4fd1df82a668
Mario23junior <mariojunior3251@gmail.com>
2020-08-16 15:16:31 -0300

    criando componente de formulario para servicos

--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ServicePrestadoFormComponent } from './service-prestado-form/service-prestado-form.component';
+import { ServicePrestadoListaComponent } from './service-prestado-lista/service-prestado-lista.component';
+ 
+const routes: Routes = [
+  {path: 'servico-prestado-form', component: ServicePrestadoFormComponent},
+  {path: 'servico-prestado-listagem', component: ServicePrestadoListaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
