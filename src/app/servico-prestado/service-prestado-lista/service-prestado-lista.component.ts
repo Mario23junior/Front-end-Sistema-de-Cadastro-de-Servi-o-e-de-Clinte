33b642c9a6c182e7640a3eb3e929d6a19a860a85
Mario23junior <mariojunior3251@gmail.com>
2020-08-19 13:32:43 -0300

    Criando template de pesquisa de clientes

--- src/app/servico-prestado/service-prestado-lista/service-prestado-lista.component.ts
+++ src/app/servico-prestado/service-prestado-lista/service-prestado-lista.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicePrestadoListaComponent implements OnInit {
 
-  constructor() { }
+  nome: String;
+  mes: number;
+  meses:number[];
+
+
+  constructor() { 
+    this.meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12]
+  }
 
   ngOnInit(): void {
   }
-
+  
+  consultar() {
+    console.log(this.nome, this.mes)
+  }
 }
