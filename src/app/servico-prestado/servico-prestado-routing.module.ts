a50c6581df33fa7d434a29b9561bb43b3e7872e2
Mario23junior <mariojunior3251@gmail.com>
2020-08-21 00:53:56 -0300

    remodelando nabegacao de rotas / criacao do template da tela de login

--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ServicePrestadoFormComponent } from './service-prestado-form/service-prestado-form.component';
 import { ServicePrestadoListaComponent } from './service-prestado-lista/service-prestado-lista.component';
+import { LayoutComponent } from '../layout/layout.component';
  
 const routes: Routes = [
-  {path: 'servico-prestado-form', component: ServicePrestadoFormComponent},
-  {path: 'servico-prestado-listagem', component: ServicePrestadoListaComponent}
+  {path: 'servicos-prestados', component: LayoutComponent, children:[
+    {path: 'form', component: ServicePrestadoFormComponent},
+    {path: 'lista', component: ServicePrestadoListaComponent},
+    {path: '', redirectTo : '/servicos-prestados/lista', pathMatch: 'full'}
+  ]}
 ];
 
 @NgModule({
